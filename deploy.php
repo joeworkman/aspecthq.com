e3964121c197e008adfb68707b652b4aba29826c
Joe Workman <joe@workmanmail.com>
2019-04-17 12:27:39 -0700

    fixed deploy

--- deploy.php
+++ deploy.php
@@ -2,6 +2,6 @@
 // This script relies on a cron job that will check for the existence of a file.
 // Then the cron job will need to pull down the latest changes via git.
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $file = '/home/joeworkman/deploy-hooks/aspecthq.com/deploy';
+    $file = 'deploy-hook';
     file_put_contents($file, json_encode($_POST, JSON_PRETTY_PRINT));
 }
