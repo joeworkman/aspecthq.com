c66b6d9b7128c8aa20567bbf62ea307fbc6c016b
Joe Workman <joe@workmanmail.com>
2022-01-14 18:06:24 -0800

    Delete deploy.php

--- deploy.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This script relies on a cron job that will check for the existence of a file.
-// Then the cron job will need to pull down the latest changes via git.
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $file = 'deploy-hook';
-    file_put_contents($file, json_encode($_POST, JSON_PRETTY_PRINT));
-}
